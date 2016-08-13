1c0e00cee8cba1830ef153136967ef7c3569768f
Jesper Dalberg <jda@fullrate.dk>
2016-08-14 00:56:55 +0200

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Digestex.Mixfile do
 
   def project do
     [app: :digestex,
-     version: "0.0.2",
+     version: "0.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
