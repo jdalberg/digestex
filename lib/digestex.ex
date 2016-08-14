6910c69f0162f249ce77f9919917fee6e3b6420f
Jesper Dalberg <jda@fullrate.dk>
2016-08-14 15:50:29 +0200

    Started preparations for Basic auth, and no auth

--- lib/digestex.ex
+++ lib/digestex.ex
@@ -3,11 +3,11 @@ defmodule Digestex do
   @methods [get: "GET", post: "POST"]
 
   def digest(url,user,password) do
-    do_digest(url,:get,user,password)
+    do_auth(url,:get,user,password)
   end
 
   def digest(url,:get,user,password) do
-    do_digest(url,:get,user,password,"")
+    do_auth(url,:get,user,password,"")
   end
 
   def digest(_url,:post,_user,_password) do
@@ -19,22 +19,22 @@ defmodule Digestex do
   end
 
   def digest(url,:post,user,password,data) do
-    do_digest(url,:post,user,password,data)
+    do_auth(url,:post,user,password,data)
   end
 
   def digest(url,:get,user,password,_data) do
-    do_digest(url,:get,user,password,"")
+    do_auth(url,:get,user,password,"")
   end
 
   def digest(_url,_method, _user, _password, _data) do
     {:error, "Method not supported"}
   end
 
-  defp do_digest(url,:get,user,password) do
-    do_digest(url,:get,user,password,"")
+  defp do_auth(url,:get,user,password) do
+    do_auth(url,:get,user,password,"")
   end
 
-  defp do_digest(url,method,user,password,data) when is_binary(data) do
+  defp do_auth(url,method,user,password,data) when is_binary(data) do
     url = to_char_list(url)
     uri = URI.parse(to_string(url))
 
@@ -46,45 +46,68 @@ defmodule Digestex do
     response = :httpc.request(method,request,[],[])
     case response do
       {:ok,{{_,401,_},fields,_}} ->
-        {realm, nonce, nc, cnonce, resp, opaque} = calcResponse(fields, user, password, uri.path, @methods[method])
-        p=%{
-          "username" => q(user),
-          "realm" => q(realm),
-          "nonce" => q(nonce),
-          "uri" => q(uri.path),
-          "qop" => "auth",
-          "nc" => nc,
-          "cnonce" => q(cnonce),
-          "response" => q(resp)
-        }
-        p=if is_bitstring(opaque) do
-          Map.put(p,"opaque",q(opaque))
-        else
-          p
-        end
-        l=for {key,val} <- p, into: [], do: key <> "=" <> val
-        authHeader=[{'Authorization', String.to_char_list("Digest " <> Enum.join(l,", "))}]
-        # have to set cookie if there is a set-cookie in the initial response
-        cookie = case List.keyfind(fields,'set-cookie',0) do
-          {'set-cookie',cookieval} -> [{'Cookie',cookieval}]
-          _ -> []
-        end
-        authHeader = authHeader ++ cookie
+        # Digest?
+        case List.keyfind(fields,'www-authenticate',0) do
+          {'www-authenticate', www_authenticate} ->
+            {aRes, auth_response} = case String.split(to_string(www_authenticate)," ") do
+              ["Digest" | auth_string] -> digest_auth_response( www_authenticate, user, password, uri.path, @methods[method] )
+              ["Basic" | _auth_string] -> basic_auth_response( user, password )
+              _ -> {:error, "Unknown WWW-Authenticate header: #{www_authenticate}"}
+            end
+            case aRes do
+              :ok ->
+                authHeader=[{'Authorization', auth_response}]
+                # have to set cookie if there is a set-cookie in the initial response
+                cookie = case List.keyfind(fields,'set-cookie',0) do
+                  {'set-cookie',cookieval} -> [{'Cookie',cookieval}]
+                  _ -> []
+                end
+                authHeader = authHeader ++ cookie
 
-        req=case method do
-          :post -> {url,authHeader,'application/x-www-form-urlencoded',String.to_char_list(data)}
-          _ -> {url,authHeader}
+                req=case method do
+                  # TODO: type shold be configurable
+                  :post -> {url,authHeader,'application/x-www-form-urlencoded',String.to_char_list(data)}
+                  _ -> {url,authHeader}
+                end
+                :httpc.request(method,req,[],[])
+              _ -> {:error, auth_response}
+            end
+          _ -> {:error, "401, but not WWW-Authenticate header found"}
         end
-        :httpc.request(method,req,[],[])
       {:ok,_} -> response
       {:error,err} -> {:error, inspect err}
     end
   end
 
-  def calcResponse(fields, user, password, uri, method, cnonce \\ nil) do
-    digestline=to_string(Enum.into(fields, %{})['www-authenticate'])
+  defp basic_auth_response( user, password ) do
+    {:error, "no basic yet"}
+  end
+
+  defp digest_auth_response( auth_string, user, password, uri_path, method ) do
+    {realm, nonce, nc, cnonce, resp, opaque} = calcResponse(auth_string, user, password, uri_path, method)
+    p=%{
+      "username" => q(user),
+      "realm" => q(realm),
+      "nonce" => q(nonce),
+      "uri" => q(uri_path),
+      "qop" => "auth",
+      "nc" => nc,
+      "cnonce" => q(cnonce),
+      "response" => q(resp)
+     }
+     p=if is_bitstring(opaque) do
+       Map.put(p,"opaque",q(opaque))
+     else
+       p
+     end
+     l=for {key,val} <- p, into: [], do: key <> "=" <> val
+     {:ok,String.to_char_list("Digest " <> Enum.join(l,", "))}
+  end
+
+  def calcResponse(digestline, user, password, uri, method, cnonce \\ nil) do
+    #digestline=to_string(Enum.into(fields, %{})['www-authenticate'])
     matched=Regex.compile!(~s/([a-z]+)=("(.*?)"|([^"]+))(,|$)/)
-    |> Regex.scan(digestline)
+    |> Regex.scan(to_string(digestline))
     dp=for e <- matched, into: %{} do
       case e do
         [_,key,_quotedvalue,"",val,_] -> {key,val}
--- test/mufasa_test.exs
+++ test/mufasa_test.exs
@@ -3,15 +3,15 @@ defmodule DigestexMufasaTest do
   doctest Digestex
 
   test "mufasa" do
-    fields = [{'connection', 'Keep-Alive'}, {'date', 'Wed, 13 Jan 2016 08:37:50 GMT'},
-             {'server', 'Apache/2.0.54 (Debian GNU/Linux) DAV/2 SVN/1.3.2'},
-             {'www-authenticate', 'Digest realm="testrealm@host.com", nonce="dcd98b7102dd2f0e8b11d0f600bfb0c093", qop="auth,auth-int", opaque="5ccc069c403ebaf9f0171e9517f40e41"'},
-             {'content-length', '401'}, {'content-type', 'text/html; charset=iso-8859-1'},
-             {'keep-alive', 'timeout=15, max=100'}]
+    www_authenticate = 'Digest realm="testrealm@host.com", nonce="dcd98b7102dd2f0e8b11d0f600bfb0c093", qop="auth,auth-int", opaque="5ccc069c403ebaf9f0171e9517f40e41"'
 
-    {realm, nonce, nc, cnonce, resp, opaque} = Digestex.calcResponse(fields, "Mufasa", "Circle Of Life", "/dir/index.html", "GET", "0a4f113b")
-    IO.inspect {realm, nonce, nc, cnonce, resp, opaque}
+    {realm, nonce, nc, cnonce, resp, opaque} = Digestex.calcResponse(www_authenticate, "Mufasa", "Circle Of Life", "/dir/index.html", "GET", "0a4f113b")
     assert resp == "6629fae49393a05397450978507c4ef1"
+    assert opaque == "5ccc069c403ebaf9f0171e9517f40e41"
+    assert realm == "testrealm@host.com"
+    assert nc == "00000001"
+    assert cnonce == "0a4f113b"
+    assert nonce == "dcd98b7102dd2f0e8b11d0f600bfb0c093"
   end
 
 end
